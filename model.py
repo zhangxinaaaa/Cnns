abc14df0f11e06c85ef5e3e176d2b63f92b8b222
zhangxinaaaa <35837004+zhangxinaaaa@users.noreply.github.com>
2020-08-01 17:25:38 +0800

    Create model.py
    Network structure

---
+++ model.py
@@ -0,0 +1,170 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+class Cnns(nn.Module):
+    def __init__(self,
+                 encoder_channel_size=[64, 64, 64, 128, 128, 128, 256, 256, 256],
+                 kernel_size=3,
+                 padding=1,
+                 dropout=0.2,
+                 frame_len=512,
+                 frame_shift=128,
+                 device='cuda:0'):
+        super(Cnns, self).__init__()
+        self.encoder_channel_size, self.decoder_channel_size = encoder_channel_size[:], encoder_channel_size[:]
+        self.decoder_channel_size.reverse()  # mirror
+        self.dropout = dropout  # dropout is applied every three layers
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.frame_len = frame_len
+        self.frame_shift = frame_shift
+        self.n_fft = frame_len
+        self.device = device
+        """
+        self.real_mtx = torch.zeros(self.n_fft, self.frame_len).cuda(self.device)  # D_r
+        self.imag_mtx = torch.zeros(self.n_fft, self.frame_len).cuda(self.device)  # D_i
+        for k in range(self.n_fft):
+            self.real_mtx[:, k] = torch.cos(torch.arange(0, self.frame_len) * k * 2 * np.pi / self.frame_len)
+            self.imag_mtx[:, k] = torch.sin(torch.arange(0, self.frame_len) * k * 2 * np.pi / self.frame_len)
+        """
+
+        self.first_layer = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=self.encoder_channel_size[0], kernel_size=1),
+            nn.Tanh()
+        )
+
+        self.encoder = nn.ModuleList()
+        """encode part, reduce size and expand channels"""
+
+        for idx in range(len(self.encoder_channel_size) - 1):  # encoder
+            layer = nn.Sequential()
+            if idx % 3 == 0:
+                layer.add_module('dropout', nn.Dropout(self.dropout))
+            layer.add_module('conv_' + str(idx + 1), nn.Conv1d(in_channels=self.encoder_channel_size[idx],
+                                                               out_channels=self.encoder_channel_size[idx + 1],
+                                                               kernel_size=self.kernel_size, stride=2,
+                                                               padding=self.padding))
+            layer.add_module('relu', nn.ReLU())
+            self.encoder.append(layer)
+
+        self.decoder = nn.ModuleList()
+        """decode part, expand size and reduce channels, mirror with encode"""
+
+        for idx in range(len(self.decoder_channel_size) - 1):  # decoder
+            layer = nn.Sequential()
+            if idx == 0:
+                double = 1
+            else:
+                double = 2
+            if idx % 3 == 0:
+                layer.add_module('dropout', nn.Dropout(self.dropout))
+            layer.add_module('conv_' + str(idx + 1), nn.Conv1d(in_channels=self.decoder_channel_size[idx] * double,
+                                                               # cat from encode, so double the input channel
+                                                               out_channels=self.decoder_channel_size[idx + 1],
+                                                               kernel_size=self.kernel_size, stride=1))
+            layer.add_module('relu', nn.ReLU())
+            self.decoder.append(layer)
+
+        self.last_layer = nn.Sequential(
+            nn.Conv1d(in_channels=self.decoder_channel_size[-1] * 2, out_channels=1, kernel_size=1),
+            nn.Tanh()
+        )
+        self.weight_init()
+
+    def forward(self, inputs):
+        skip = []
+        layer_in = self.first_layer(inputs)
+        skip.append(layer_in)
+
+        """encode forward part"""
+        for layer_idx, encode_layer in enumerate(self.encoder):
+            skip.append(layer_in)
+            layer_out = encode_layer(layer_in)
+            layer_in = layer_out
+
+        """decode forward part"""
+        for layer_idx, decode_layer in enumerate(self.decoder):
+            """insert zeros between the consecutive samples to up sample"""
+            expand_layer = torch.zeros((layer_in.shape[0], layer_in.shape[1],
+                                        layer_in.shape[2] * 2 - 1 + self.padding * 2 + 1)).cuda(self.device)
+            expand_layer[:, :, [i for i in range(self.padding, expand_layer.shape[2] - self.padding - 1, 2)]] \
+                = layer_in
+            layer_out = decode_layer(expand_layer)
+            layer_out = torch.cat((layer_out, skip[-layer_idx - 1]),
+                                  dim=1)  # skip cat in channel dim
+            layer_in = layer_out
+
+        wave_out = self.last_layer(layer_in)
+        mag_out = self.wave2spec(wave_out)  # convert wave to mag spec using torch.stft
+        return wave_out, mag_out
+
+    def wave2spec(self, wave_out):
+        spec_out = torch.stft(wave_out.squeeze(), n_fft=self.n_fft, hop_length=self.frame_shift,
+                              win_length=self.frame_len, window=torch.hann_window(self.frame_len).cuda(self.device),
+                              center=False).cuda(self.device)
+        # mag = sqrt(real_part ** 2 + imag_part ** 2)
+        mag_out = torch.sqrt(torch.sum(spec_out ** 2, -1))
+        return mag_out
+
+    def weight_init(self):
+        for child in self.first_layer:
+            if isinstance(child, nn.Conv1d):
+                nn.init.xavier_normal_(child.weight)
+        for module in self.encoder:
+            for child in module.children():
+                if isinstance(child, nn.Conv1d):
+                    nn.init.xavier_normal_(child.weight)
+        for module in self.decoder:
+            for child in module.children():
+                if isinstance(child, nn.Conv1d):
+                    nn.init.xavier_normal_(child.weight)
+        for child in self.first_layer:
+            if isinstance(child, nn.Conv1d):
+                nn.init.xavier_normal_(child.weight)
+
+    def out2mag(self, outputs):
+        """using transform matrix to convert output to magnitude while keeping it differential"""
+        n_frames = (outputs.shape[-1] - self.frame_len) // self.frame_shift + 1
+        mag = torch.zeros(outputs.shape[0], n_frames, self.n_fft // 2 + 1).cuda(self.device)
+        for sequence in range(outputs.shape[0]):
+            for frame_idx in range(n_frames):
+                frame = outputs[sequence, :, frame_idx * self.frame_shift:frame_idx * self.frame_shift + self.frame_len]
+                frame = frame * torch.hann_window(self.frame_len).cuda(self.device)
+                real_part = torch.mm(frame.view(1, -1), self.real_mtx).squeeze()
+                imag_part = torch.mm(frame.view(1, -1), self.imag_mtx).squeeze()
+                real_stft = real_part[:self.n_fft // 2 + 1]
+                imag_stft = imag_part[:self.n_fft // 2 + 1]
+                mag[sequence, frame_idx, :] = torch.sqrt(real_stft ** 2 + imag_stft ** 2 + 1e-12)  # get magnitude
+
+        return mag
+
+
+if __name__ == "__main__":
+    device = 'cuda:0'
+    # in_features = torch.randn(32, 1, 2048).cuda(device)
+    in_features = torch.randn(32, 1, 16384).cuda(device)
+    net = Cnns(kernel_size=11, padding=5).cuda(device)
+    print('wang xu shi xiao zhu')
+    """
+     net = Tcn(input_size=257,
+              n_output=257,
+              d_model=257,
+              k=3,
+              max_d_rate=16,
+              n_blocks=40,
+              d_f=64).cuda(device)
+    
+    """
+    net_info = get_parameter_number(net)
+    print(net_info)
+    # print(net.parameters)
+    out, mag = net(in_features)
+    print(out.shape)
+    print(mag.shape)
